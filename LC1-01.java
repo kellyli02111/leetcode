e146b14a050e7cb0a034f784ecc81e4f23ad8a35
Kelly Li <kellyli0211@gmail.com>
2018-10-15 15:35:06 -0700

    Create LC1-01.java

---
+++ LC1-01.java
@@ -0,0 +1,23 @@
+// time: O(n^2)
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums.length == 0)
+            return nums;
+        int[] result = new int[]{0,0};
+        for (int i = 0; i < nums.length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
