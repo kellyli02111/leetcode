e861f28df762eab80ef7301bc07d11d6ad4f64e1
Kelly Li <kellyli0211@gmail.com>
2019-03-05 18:13:56 -0800

    Create LC26-01.java

---
+++ LC26-01.java
@@ -0,0 +1,33 @@
+//O(n)?
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        int start = 0;
+        int next = start + 1;
+        int move = next;
+        int end = nums[nums.length - 1];
+        while (move < nums.length && nums[start] < end) {
+            if (nums[next] <= nums[start]) {
+                
+                while (move < nums.length && nums[move] <= nums[start]) {
+                    move++;
+                }
+                if (move < nums.length) {
+                    int temp = nums[next];
+                    nums[next] = nums[move];
+                    nums[move] = temp;
+                }
+            }
+            start++;
+            next = start + 1;
+            move = next;
+        }
+        return next;
+    }
+}
