48f21f48f616dc08c0a81e75a140f79493b35397
Kelly Li <kellyli0211@gmail.com>
2019-03-05 18:23:34 -0800

    Update LC26-02.java

--- LC26-02.java
+++ LC26-02.java
@@ -5,9 +5,9 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        if (nums.length == 1) {
-            return 1;
-        }
+        // if (nums.length == 1) {
+            // return 1;
+        // }
         int i = 0;
         
         for (int j = 1; j < nums.length; j++) {
