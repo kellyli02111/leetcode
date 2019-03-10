8deb4af7a02fde3404bcafcbb1f6c5d7e8cb1ef1
Kelly Li <kellyli0211@gmail.com>
2019-03-09 16:45:35 -0800

    Create LC42-02.java

---
+++ LC42-02.java
@@ -0,0 +1,36 @@
+// time: O(n)
+// space: O(1)
+
+class Solution {
+    public int trap(int[] height) {
+        if (height.length < 3) {
+            return 0;
+        }
+        int trappedSize = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] > leftMax) {
+                    leftMax = height[left];
+                }
+                else {
+                    trappedSize += leftMax - height[left];
+                }
+                left++;
+            }
+            else {
+                if (height[right] > rightMax) {
+                    rightMax = height[right];
+                }
+                else {
+                    trappedSize += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return trappedSize;
+    }
+}
