2eee97013b4c5974a9ea5b80e9c2843a344b3206
Kelly Li <kellyli0211@gmail.com>
2019-03-20 15:46:27 -0700

    Create LC19-02.java

---
+++ LC19-02.java
@@ -0,0 +1,31 @@
+//one pass
+//two pointers
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode second = dummy;
+        ListNode first = dummy;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while (first.next != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
